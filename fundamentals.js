8313d859562ac738dcf4a27735713e23236f864f
Jiahan Chen <3238468729@qq.com>
2023-09-12 11:29:01 +0100

    Loop constructs.

--- fundamentals.js
+++ fundamentals.js
@@ -1,46 +1,50 @@
 const aCar = {
-    owner : "Joe Bloggs",
-    address : '3 Walkers Lane',
-    previous_owners: [
-      {name: 'Pat Smith', address: '1 Main Street'},
-      {name: 'Sheila Dwyer', address: '2 High Street'},
-    ],
-    type : {
-      make: "Toyota",
-      model: "Corolla",
-      cc: 1.8
-    },
-    features : ['Parking assist', 'Alarm', 'Tow-bar'],
-    registration : {
-      year: 201,
-      county: "WD",
-      number: 1058 
-    }
-  };
-  
-  console.log(aCar.owner + ' drives a ' + aCar.type.make);
-  console.log("Reg. = " + aCar.registration.year + '-' + aCar.registration.county + '-' + aCar.registration.number);
-
-  aCar.mileage = 10000;
-  aCar.color = {
-    exterior : "red",
-    interior : {
-      texture: "leather",
-      shade: "cream"
-    }
+  owner : "Joe Bloggs",
+  address : '3 Walkers Lane',
+  previous_owners: [
+    {name: 'Pat Smith', address: '1 Main Street'},
+    {name: 'Sheila Dwyer', address: '2 High Street'},
+  ],
+  type : {
+    make: "Toyota",
+    model: "Corolla",
+    cc: 1.8
+  },
+  features : ['Parking assist', 'Alarm', 'Tow-bar'],
+  registration : {
+    year: 201,
+    county: "WD",
+    number: 1058 
   }
+};
 
-  console.log(
-    "It is a " + aCar.color.exterior + " car, " + aCar.mileage + 
-    " mileage, with " + aCar.color.interior.texture + " interior."
-  );
+console.log(aCar.owner + ' drives a ' + aCar.type.make);
+console.log("Reg. = " + aCar.registration.year + '-' + aCar.registration.county + '-' + aCar.registration.number);
 
-  console.log('First owner : ' + aCar.previous_owners[0].name + ' - ' + aCar.previous_owners[0].address)
-
-  for (let i = 0 ; i < aCar.features.length ; i += 1) {
-    console.log(aCar.features[i]) ;
+aCar.mileage = 10000;
+aCar.color = {
+  exterior : "red",
+  interior : {
+    texture: "leather",
+    shade: "cream"
   }
+}
 
-  for (let i = 0 ; i < aCar.registration.length ; i++){
-    console.log(aCar.previous_owners[i].name);
-  }
+console.log(
+  "It is a " + aCar.color.exterior + " car, " + aCar.mileage + 
+  " mileage, with " + aCar.color.interior.texture + " interior."
+);
+
+console.log('First owner : ' + aCar.previous_owners[0].name + ' - ' + aCar.previous_owners[0].address)
+
+for (let i = 0 ; i < aCar.features.length ; i += 1) {
+  console.log(aCar.features[i]) ;
+}
+
+for (let i = 0 ; i < aCar.registration.length ; i++){
+  console.log(aCar.previous_owners[i].name);
+}
+
+for (let p in aCar.type)  {
+  console.log(p.toUpperCase() + ' = ' + aCar.type[p] ) ;
+}
